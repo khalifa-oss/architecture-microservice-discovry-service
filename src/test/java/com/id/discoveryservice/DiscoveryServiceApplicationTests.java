8d5f569e622a6eb06852a96e6550305f65bfa438
khalifa-oss <saidikhalifa22@yahoo.fr>
2022-11-08 09:12:50 +0100

    initialize commit

---
+++ src/test/java/com/id/discoveryservice/DiscoveryServiceApplicationTests.java
@@ -0,0 +1,15 @@
+package com.id.discoveryservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@SpringBootTest
+
+class DiscoveryServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
